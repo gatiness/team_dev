b1ec17b21b953c8d6a5cb187a7f6bb85ad2db0ff
MachikoFujii <machiko.fujii@gmail.com>
2021-06-12 15:04:30 +0900

    addied authenticate user function, email function revised

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,8 +1,26 @@
 class AgendasController < ApplicationController
   # before_action :set_agenda, only: %i[show edit update destroy]
 
+  before_action :set_agenda, only: %i[destroy]
+
   def index
     @agendas = Agenda.all
+  end
+
+  def destroy 
+    path = Rails.application.routes.recognize_path(request.refer)
+    @agenda.destroy
+    redirect_to path
+    @team = @agenda.team
+    @users = @team.members
+
+    if current_user.id == @agenda.user_id || current_user.id == @team.owner_id
+      @aggenda.destroy
+      assginMailer.addign_mail(@users).deliver
+      redirect-to dashboard_path
+    else
+      redirect_to path
+    end
   end
 
   def new
--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -6,4 +6,14 @@ class AssignMailer < ApplicationMailer
     @password = password
     mail to: @email, subject: I18n.t('views.messages.complete_registration')
   end
+  # def assign_mail(email, password)
+  #   @email = email
+  #   @password = password
+  #   mail to: @email, subject: I18n.t('views.messages.complete_registration')
+  # end
+  def assign_mail(users)
+
+    @email = users.pluck(users)
+
+    mail to : @email.split(","), subject: I18n.t('views.messages.complete_registration')
 end
