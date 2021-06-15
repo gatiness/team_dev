8c8846a8557da851441fc484ddae936bad20a045
MachikoFujii <machiko.fujii@gmail.com>
2021-06-15 11:13:45 +0900

    comppleting this phase

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,8 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
-  def edit; end
+  def edit; 
+  end
 
   def create
     @team = Team.new(team_params)
@@ -33,6 +34,10 @@ class TeamsController < ApplicationController
     if current_user == @team.owner
       if @team.update(team_params)
         redirect_to @team, notice: I18n.t('views.messages.update_team')
+      else
+        flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+        render :edit
+      end
     else
       render :edit
     end
@@ -70,3 +75,4 @@ class TeamsController < ApplicationController
     params.permit(:owner_id)
   end
 end
+
